4a8a4637ebb823a8cc14cdc1979b455a6b42e061
Bryce Cordill <brycecordill@gmail.com>
2019-09-12 19:24:40 -0700

    Implemented case where tab between people has not yet been created

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,55 @@
 use std::error::Error;
 use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::fs::OpenOptions;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let data = fs::read_to_string(config.filename)?;
+    if !Path::new(&config.filename).exists() {
+        File::create(&config.filename)?;
+    }
+    let data = fs::read_to_string(&config.filename)?;
+
+    // 'Sort' the name combo so it is consistent
+    let name_combo = if config.name1 < config.name2 {
+        format!("{}_{}", config.name1, config.name2)
+    } else {
+        format!("{}_{}", config.name1, config.name2)
+    };
+
+    let i = match data.find(&name_combo) {
+        Some(num) => num,
+        None => return append_new(&config, &name_combo),
+    };
 
     Ok(())
+}
+
+fn append_new(config: &Config, name_combo: &str) -> Result<(), Box<dyn Error>> {
+    let mut file = OpenOptions::new().append(true).open(&config.filename)?;
+
+    let new_line = format!("{} {}", name_combo, calculate_tab(&config));
+
+    file.write(&new_line.as_bytes())?;
+    Ok(())
+}
+
+// TODO Comment this or rewrite to make more clear
+fn calculate_tab(config: &Config) -> f64 {
+    assert!(config.action == "recv" || config.action == "owes");
+
+    if config.action == "recv" {
+        if config.name1 < config.name2 {
+            return config.amount
+        }
+        config.amount * -1.0
+    } else {  // If action == "owes"
+        if config.name1 < config.name2 {
+            return config.amount * -1.0
+        }
+        config.amount
+    }
 }
 
 pub struct Config {
@@ -36,6 +81,9 @@ impl Config {
             Some(arg) => arg,
             None => return Err("No action given")
         };
+        if action != "recv" || action != "owes" {
+            return Err("Invalid action")
+        }
 
         let name2 = match args.next() {
             Some(arg) => arg.to_lowercase(),
